4dc4b8a9fe15c7932daf2d1550d8624c0a2cdd81
Dmitriy <mr.priskoka@yandex.com>
2020-11-16 12:03:12 +0300

    Added product list loading in product ViewModel.

--- app/src/main/java/pw/prsk/goodfood/ProductsFragment.kt
+++ app/src/main/java/pw/prsk/goodfood/ProductsFragment.kt
@@ -7,15 +7,27 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import pw.prsk.goodfood.adapters.ProductAdapter
+import pw.prsk.goodfood.data.AppDatabase
+import pw.prsk.goodfood.databinding.DialogAddProductBinding
 import pw.prsk.goodfood.databinding.FragmentProductsBinding
+import pw.prsk.goodfood.repository.ProductRepository
 import pw.prsk.goodfood.viewmodels.ProductsViewModel
 
-class ProductsFragment: Fragment() {
+class ProductsFragment : Fragment() {
     private var _binding: FragmentProductsBinding? = null
     private val binding get() = _binding!!
 
     private val viewModel: ProductsViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val dbInstance = AppDatabase.getInstance(requireContext().applicationContext)
+        val repository = ProductRepository(dbInstance)
+        viewModel.injectRepository(repository)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -29,9 +41,25 @@ class ProductsFragment: Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val productAdapter = ProductAdapter()
+        subscribeUi(productAdapter)
+
         binding.rvProductsList.apply {
             layoutManager = LinearLayoutManager(this.context)
-            adapter = ProductAdapter()
+            adapter = productAdapter
+        }
+
+        binding.fabAddProduct.setOnClickListener {
+            val bsd = BottomSheetDialog(requireContext(), R.style.BottomSheetDialogStyle)
+            val dialogBinding = DialogAddProductBinding.inflate(layoutInflater)
+            bsd.setContentView(dialogBinding.root)
+            bsd.show()
+        }
+    }
+
+    fun subscribeUi(adapter: ProductAdapter) {
+        viewModel.productsList.observe(viewLifecycleOwner) { list ->
+            adapter.setList(list)
         }
     }
 
--- app/src/main/java/pw/prsk/goodfood/viewmodels/ProductsViewModel.kt
+++ app/src/main/java/pw/prsk/goodfood/viewmodels/ProductsViewModel.kt
@@ -1,6 +1,31 @@
 package pw.prsk.goodfood.viewmodels
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+import pw.prsk.goodfood.data.Product
+import pw.prsk.goodfood.repository.ProductRepository
 
 class ProductsViewModel: ViewModel() {
+    private var repository: ProductRepository? = null
+
+    val productsList: MutableLiveData<List<Product>> by lazy {
+        MutableLiveData<List<Product>>()
+    }
+
+    fun loadProductsList() {
+        viewModelScope.launch {
+            productsList.postValue(repository?.getProducts())
+        }
+    }
+
+    fun injectRepository(repository: ProductRepository) {
+        this.repository = repository
+        loadProductsList()
+    }
+
+    override fun onCleared() {
+        repository = null
+    }
 }
