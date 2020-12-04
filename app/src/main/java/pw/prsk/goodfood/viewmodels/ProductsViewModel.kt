7eac1086f10a9b165d646aec499f3a5a1d93422d
Dmitriy <mr.priskoka@yandex.com>
2020-12-04 14:09:42 +0300

    Added ProductCategoryRepository.

--- app/src/main/java/pw/prsk/goodfood/viewmodels/ProductsViewModel.kt
+++ app/src/main/java/pw/prsk/goodfood/viewmodels/ProductsViewModel.kt
@@ -3,11 +3,13 @@ package pw.prsk.goodfood.viewmodels
 import androidx.lifecycle.*
 import kotlinx.coroutines.launch
 import pw.prsk.goodfood.data.Product
+import pw.prsk.goodfood.repository.ProductCategoryRepository
 import pw.prsk.goodfood.repository.ProductRepository
 import pw.prsk.goodfood.utils.ItemTouchHelperAction
 
 class ProductsViewModel : ViewModel(), ItemTouchHelperAction {
-    private var repository: ProductRepository? = null
+    private var productRepository: ProductRepository? = null
+    private var productCategoryRepository: ProductCategoryRepository? = null
 
     val productsList: MutableLiveData<List<Product>> by lazy {
         MutableLiveData<List<Product>>()
@@ -15,30 +17,35 @@ class ProductsViewModel : ViewModel(), ItemTouchHelperAction {
 
     fun addProduct(product: Product) {
         viewModelScope.launch {
-            repository!!.addProduct(product)
+            productRepository!!.addProduct(product)
             loadProductsList()
         }
     }
 
     private fun loadProductsList() {
         viewModelScope.launch {
-            productsList.postValue(repository!!.getProducts())
+            productsList.postValue(productRepository!!.getProducts())
         }
     }
 
-    fun injectRepository(repository: ProductRepository) {
-        this.repository = repository
+    fun injectRepository(
+        productRepository: ProductRepository,
+        productCategoryRepository: ProductCategoryRepository
+    ) {
+        this.productRepository = productRepository
+        this.productCategoryRepository = productCategoryRepository
         loadProductsList()
     }
 
     override fun onCleared() {
-        repository = null
+        productRepository = null
+        productCategoryRepository = null
     }
 
     override fun itemSwiped(position: Int, direction: Int) {
         viewModelScope.launch {
             val item = productsList.value?.get(position)
-            repository?.removeProduct(item!!)
+            productRepository?.removeProduct(item!!)
             loadProductsList()
         }
     }
