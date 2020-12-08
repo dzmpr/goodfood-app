6b4247d4d37afce5193837c98b2b7a7a28c35c7c
Dmitriy <mr.priskoka@yandex.com>
2020-12-08 14:08:36 +0300

    Inject repositories to MealsViewModel.

--- app/src/main/java/pw/prsk/goodfood/di/components/AppComponent.kt
+++ app/src/main/java/pw/prsk/goodfood/di/components/AppComponent.kt
@@ -2,18 +2,18 @@ package pw.prsk.goodfood.di.components
 
 import android.app.Application
 import dagger.Component
-import pw.prsk.goodfood.MealsFragment
-import pw.prsk.goodfood.ProductsFragment
-import pw.prsk.goodfood.data.AppDatabase
 import pw.prsk.goodfood.di.modules.ApplicationModule
 import pw.prsk.goodfood.di.modules.RepositoryModule
 import pw.prsk.goodfood.di.modules.RoomModule
+import pw.prsk.goodfood.viewmodels.MealsViewModel
+import pw.prsk.goodfood.viewmodels.ProductsViewModel
 import javax.inject.Singleton
 
 @Singleton
 @Component(modules = [ApplicationModule::class, RepositoryModule::class, RoomModule::class])
 interface AppComponent {
     fun context(): Application
-    fun inject(fragment: MealsFragment)
-    fun inject(fragment: ProductsFragment)
+
+    fun inject(viewModel: MealsViewModel)
+    fun inject(viewModel: ProductsViewModel)
 }
--- app/src/main/java/pw/prsk/goodfood/viewmodels/MealsViewModel.kt
+++ app/src/main/java/pw/prsk/goodfood/viewmodels/MealsViewModel.kt
@@ -1,6 +1,5 @@
 package pw.prsk.goodfood.viewmodels
 
-import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
@@ -9,6 +8,7 @@ import pw.prsk.goodfood.utils.ItemTouchHelperAction
 import pw.prsk.goodfood.data.Meal
 import pw.prsk.goodfood.repository.MealCategoryRepository
 import pw.prsk.goodfood.repository.MealRepository
+import javax.inject.Inject
 
 
 class MealsViewModel : ViewModel(), ItemTouchHelperAction {
@@ -16,40 +16,29 @@ class MealsViewModel : ViewModel(), ItemTouchHelperAction {
         MutableLiveData<List<Meal>>()
     }
 
-    private var mealRepository: MealRepository? = null
-    private var mealCategoryRepository: MealCategoryRepository? = null
+    @Inject lateinit var mealRepository: MealRepository
+    @Inject lateinit var mealCategoryRepository: MealCategoryRepository
 
     fun addMeal(meal: Meal) {
         viewModelScope.launch {
-            mealRepository!!.addMeal(meal)
+            mealRepository.addMeal(meal)
             loadMealsList()
         }
     }
 
-    private fun loadMealsList() {
+    fun loadMealsList() {
         viewModelScope.launch {
-            mealList.postValue(mealRepository!!.getMeals())
+            mealList.postValue(mealRepository.getMeals())
         }
-    }
-
-    fun injectRepositories(mealRepository: MealRepository, mealCategoryRepository: MealCategoryRepository) {
-        this.mealRepository = mealRepository
-        this.mealCategoryRepository = mealCategoryRepository
-        loadMealsList()
     }
 
     override fun itemSwiped(position: Int, direction: Int) {
         viewModelScope.launch {
             val item = mealList.value?.get(position)
-            mealRepository?.removeMeal(item!!)
+            mealRepository.removeMeal(item!!)
             loadMealsList()
         }
     }
 
     override fun itemMoved(startPosition: Int, endPosition: Int) {}
-
-    override fun onCleared() {
-        mealRepository = null
-        mealCategoryRepository = null
-    }
 }
