c3d4a407749adb98c17f33a3dfbdbbedfdf865f5
Dmitriy <mr.priskoka@yandex.com>
2021-03-08 10:21:51 +0300

    Added ViewModelFactory to inject dependencies to ViewModels.

--- app/src/main/java/pw/prsk/goodfood/di/components/AppComponent.kt
+++ app/src/main/java/pw/prsk/goodfood/di/components/AppComponent.kt
@@ -2,21 +2,28 @@ package pw.prsk.goodfood.di.components
 
 import android.app.Application
 import dagger.Component
-import pw.prsk.goodfood.di.modules.ApplicationModule
-import pw.prsk.goodfood.di.modules.GatewayModule
-import pw.prsk.goodfood.di.modules.RepositoryModule
-import pw.prsk.goodfood.di.modules.RoomModule
+import pw.prsk.goodfood.RecipesOverviewFragment
+import pw.prsk.goodfood.di.modules.*
 import pw.prsk.goodfood.viewmodels.EditRecipeViewModel
 import pw.prsk.goodfood.viewmodels.RecipesOverviewViewModel
 import pw.prsk.goodfood.viewmodels.ProductsViewModel
 import javax.inject.Singleton
 
 @Singleton
-@Component(modules = [ApplicationModule::class, RepositoryModule::class, RoomModule::class, GatewayModule::class])
+@Component(modules = [
+    ApplicationModule::class,
+    RepositoryModule::class,
+    RoomModule::class,
+    GatewayModule::class,
+    ViewModelModule::class
+]
+)
 interface AppComponent {
     fun context(): Application
 
-    fun inject(viewModel: RecipesOverviewViewModel)
+    fun inject(fragment: RecipesOverviewFragment)
+
+//    fun inject(viewModel: RecipesOverviewViewModel)
     fun inject(viewModel: ProductsViewModel)
     fun inject(viewModel: EditRecipeViewModel)
 }
