1cd0f16317098d4649d587be979faf067fa66915
Dmitriy <mr.priskoka@yandex.com>
2021-03-12 16:13:24 +0300

    Added toolbar and themes for it.

--- app/src/main/java/pw/prsk/goodfood/RecipeFragment.kt
+++ app/src/main/java/pw/prsk/goodfood/RecipeFragment.kt
@@ -4,16 +4,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.Navigation
 import pw.prsk.goodfood.databinding.FragmentRecipeBinding
 import pw.prsk.goodfood.viewmodels.RecipeViewModel
 import java.lang.IllegalStateException
 import javax.inject.Inject
 
-class RecipeFragment: Fragment() {
+class RecipeFragment : Fragment() {
     private var _binding: FragmentRecipeBinding? = null
-    private val binding get () = _binding!!
+    private val binding get() = _binding!!
 
     @Inject lateinit var vmFactory: ViewModelProvider.Factory
     private lateinit var viewModel: RecipeViewModel
@@ -45,14 +47,26 @@ class RecipeFragment: Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        setupToolbar()
+
         viewModel.recipe.observe(viewLifecycleOwner) {
-            binding.ctToolbar.title = it.name
-            binding.toolbar.title = it.name
-            binding.tvDescription.text = it.name
+//            binding.ctToolbar.title = it.name
+//            binding.toolbar.title = it.name
         }
 
         viewModel.recipePhoto.observe(viewLifecycleOwner) {
-            binding.ivRecipePhoto.setImageBitmap(it)
+//            binding.ivRecipePhoto.setImageBitmap(it)
+        }
+    }
+
+    private fun setupToolbar() {
+        binding.tbToolbar.setNavigationOnClickListener {
+            Navigation.findNavController(requireActivity(), R.id.fcvContainer).popBackStack()
+        }
+
+        binding.tbToolbar.setOnMenuItemClickListener {
+            Toast.makeText(this.context, "Test menu", Toast.LENGTH_SHORT).show()
+            true
         }
     }
 
