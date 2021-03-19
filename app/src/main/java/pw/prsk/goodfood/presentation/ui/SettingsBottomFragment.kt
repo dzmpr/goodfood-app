56b7859436a7fa24c544c0d3cc02c2743dbaf07e
Dmitriy <mr.priskoka@yandex.com>
2021-03-19 12:05:51 +0300

    Added margins to bottom sheets in landscape orientation.

--- app/src/main/java/pw/prsk/goodfood/presentation/ui/SettingsBottomFragment.kt
+++ app/src/main/java/pw/prsk/goodfood/presentation/ui/SettingsBottomFragment.kt
@@ -1,33 +1,17 @@
 package pw.prsk.goodfood.presentation.ui
 
-import android.app.Dialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.navigation.Navigation
-import com.google.android.material.bottomsheet.BottomSheetBehavior
-import com.google.android.material.bottomsheet.BottomSheetDialog
-import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import pw.prsk.goodfood.R
 import pw.prsk.goodfood.databinding.FragmentBottomSettingsBinding
 
-class SettingsBottomFragment: BottomSheetDialogFragment() {
+class SettingsBottomFragment: BaseBottomSheetFragment() {
     private var _binding: FragmentBottomSettingsBinding? = null
     private val binding get() = _binding!!
-
-    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        // Open dialog with expanded state
-        val bottomSheetDialog = super.onCreateDialog(savedInstanceState) as BottomSheetDialog
-        bottomSheetDialog.setOnShowListener {
-            val dialog = it as BottomSheetDialog
-            val bottomSheet =
-                dialog.findViewById<View>(com.google.android.material.R.id.design_bottom_sheet)
-            BottomSheetBehavior.from(bottomSheet!!).state = BottomSheetBehavior.STATE_EXPANDED
-        }
-        return bottomSheetDialog
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -39,6 +23,7 @@ class SettingsBottomFragment: BottomSheetDialogFragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
         binding.llItemManageCategories.setOnClickListener {
             Toast.makeText(context, "Not implemented", Toast.LENGTH_SHORT).show()
         }
