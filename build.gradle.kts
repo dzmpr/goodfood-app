78db5c027c5217db3244f0ba57377274d9430793
Dmitriy <mr.priskoka@yandex.com>
2021-09-12 12:13:45 +0300

    Moved gradle scripts to kotlin DSL.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,18 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
-    ext.kotlin_version = '1.5.21'
+    extra["kotlinVersion"] = "1.5.30"
+
     repositories {
         google()
         mavenCentral()
     }
-    dependencies {
-        classpath 'com.android.tools.build:gradle:7.0.0'
-        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version"
 
-        classpath "org.jetbrains.kotlin:kotlin-serialization:$kotlin_version"
+    dependencies {
+        val kotlinVersion: String by rootProject.extra
+        classpath("com.android.tools.build:gradle:7.0.2")
+
+        classpath(kotlin("gradle-plugin", version = kotlinVersion))
+        classpath(kotlin("serialization", version = kotlinVersion))
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
     }
@@ -22,6 +25,6 @@ allprojects {
     }
 }
 
-task clean(type: Delete) {
-    delete rootProject.buildDir
+tasks.register<Delete>("clean") {
+    delete(rootProject.buildDir)
 }
