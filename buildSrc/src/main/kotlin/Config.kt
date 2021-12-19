4fcdbab92d0367bee0ce4eaa9ecdf3210918d6f8
Dmitriy <mr.priskoka@yandex.com>
2021-12-19 20:09:11 +0300

    move dependencies to buildSrc module

---
+++ buildSrc/src/main/kotlin/Config.kt
@@ -0,0 +1,9 @@
+object Config {
+
+    const val minSdk = 23
+    const val targetSdk = 30
+    const val compileSdk = 31
+
+    const val buildToolsVersion = "30.0.3"
+    const val appId = "ru.cookedapp.cooked"
+}
