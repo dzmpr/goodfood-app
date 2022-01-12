bdd4ab0f184fd135203713372d4e1506b74384b3
Dmitriy <mr.priskoka@yandex.com>
2022-01-12 22:17:11 +0300

    RecipeEntity refactor

--- storage/src/main/kotlin/ru/cookedapp/storage/entity/Recipe.kt
+++ storage/src/main/kotlin/ru/cookedapp/storage/entity/Recipe.kt
@@ -4,13 +4,13 @@ import java.time.LocalDateTime
 
 data class Recipe(
     val id: Long,
-    var name: String,
-    var description: String? = null,
-    var photoFilename: String? = null,
-    var servingsNum: Int,
-    var inFavorites: Boolean = false,
-    var lastCooked: LocalDateTime,
-    var cookCount: Int = 0,
-    var ingredientsList: List<IngredientWithMeta>,
-    var category: RecipeCategoryEntity,
+    val name: String,
+    val description: String? = null,
+    val photoFilename: String? = null,
+    val servingsNum: Int,
+    val inFavorites: Boolean = false,
+    val lastCooked: LocalDateTime,
+    val cookCount: Int = 0,
+    val ingredientsList: List<IngredientWithMeta>,
+    val category: RecipeCategoryEntity,
 )
--- storage/src/main/kotlin/ru/cookedapp/storage/entity/RecipeEntity.kt
+++ storage/src/main/kotlin/ru/cookedapp/storage/entity/RecipeEntity.kt
@@ -8,14 +8,57 @@ import java.time.LocalDateTime
 @Entity(tableName = "recipes")
 data class RecipeEntity(
     @PrimaryKey(autoGenerate = true)
-    @ColumnInfo(name = "id") override val id: Long,
-    @ColumnInfo(name = "name") var name: String,
-    @ColumnInfo(name = "instructions") var instructions: String? = null,
-    @ColumnInfo(name = "photo_filename") var photoFilename: String? = null,
-    @ColumnInfo(name = "servings_num") var servingsNum: Int,
-    @ColumnInfo(name = "in_favorites") var inFavorites: Boolean = false,
-    @ColumnInfo(name = "last_cooked") var lastCooked: LocalDateTime,
-    @ColumnInfo(name = "cook_count") var cookCount: Int = 0,
-    @ColumnInfo(name = "ingredients_list") var ingredientsList: List<Ingredient>,
-    @ColumnInfo(name = "category_id") var categoryId: Long,
-) : Identifiable
+    @ColumnInfo(name = "id")
+    override val id: Long,
+
+    @ColumnInfo(name = "name")
+    val name: String,
+
+    @ColumnInfo(name = "instructions")
+    val instructions: String?,
+
+    @ColumnInfo(name = "photo_filename")
+    val photoFilename: String?,
+
+    @ColumnInfo(name = "servings_num")
+    val servingsNum: Int,
+
+    @ColumnInfo(name = "in_favorites")
+    val inFavorites: Boolean,
+
+    @ColumnInfo(name = "last_cooked")
+    val lastCooked: LocalDateTime,
+
+    @ColumnInfo(name = "cook_count")
+    val cookCount: Int,
+
+    @ColumnInfo(name = "ingredients_list")
+    val ingredientsList: List<Ingredient>,
+
+    @ColumnInfo(name = "category_id")
+    val categoryId: Long,
+) : Identifiable {
+
+    constructor(
+        name: String,
+        instructions: String? = null,
+        photoFilename: String? = null,
+        servingsNum: Int,
+        inFavorites: Boolean = false,
+        lastCooked: LocalDateTime,
+        cookCount: Int = 0,
+        ingredientsList: List<Ingredient>,
+        categoryId: Long,
+    ) : this(
+        id = 0,
+        name = name,
+        instructions = instructions,
+        photoFilename = photoFilename,
+        servingsNum = servingsNum,
+        inFavorites = inFavorites,
+        lastCooked = lastCooked,
+        cookCount = cookCount,
+        ingredientsList = ingredientsList,
+        categoryId = categoryId,
+    )
+}
